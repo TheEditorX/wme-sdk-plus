f09f4dfbcfa3a09d127cc6b85e964c5b54374ae0
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2026-02-21 19:42:18 +0200

    feat(DataModel): deprecate map comments' methods (#23)
    * feat: create named exports for transactions module
    
    * feat: add support for return values in transaction's doActions method
    
    * fix: correct reference to the SDK in updateMapComment rule
    
    * feat: add a proxy for map comment methods to use SDK impl. when possible
    
    * fix: typo in deprecation warning and message print location
    
    * fix: remove default values when updating map comments
    
    * feat: reject asynchronous callbacks in doActions
    
    * fix: lock rank was ignored if set to 0 because of a falsy check

--- packages/patch-editing--transactions/src/index.ts
+++ packages/patch-editing--transactions/src/index.ts
@@ -14,31 +14,59 @@ function getTransactionManager() {
   return transactionManager;
 }
 
+export function beginTransaction(manager: TransactionManager = getTransactionManager()): void {
+  manager.beginTransaction();
+}
+export function commitTransaction(description?: string, manager: TransactionManager = getTransactionManager()): void {
+  manager.commitTransaction(description);
+}
+export function cancelTransaction(manager: TransactionManager = getTransactionManager()): void {
+  manager.cancelTransaction();
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function doActions<T>(cb: () => T extends Promise<any> ? never : T, description?: string, manager?: TransactionManager): T {
+  beginTransaction(manager);
+  try {
+    const result = cb();
+    if (result instanceof Promise) {
+      console.error(
+        'doActions detected an async callback. Async operations are not supported because ' +
+        'actions added after the transaction closes cannot be captured. Complete async work ' +
+        'before calling doActions, or use synchronous callbacks only.\n\n' +
+
+        'The provided callback will continue to execute in this session and may have side-effects ' +
+        'on the WME session, and may have the actions generated within that callback to leak outside ' +
+        'the boundaries of the transaction into the change log.'
+      );
+      cancelTransaction(manager);
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return undefined!;
+    }
+
+    commitTransaction(description, manager);
+    return result;
+  } catch (e) {
+    cancelTransaction(manager);
+    throw e;
+  }
+}
+
 export default [
   new DefinePropertyRule(
     'Editing.beginTransaction',
-    () => getTransactionManager().beginTransaction(),
+    () => beginTransaction(),
   ),
   new DefinePropertyRule(
     'Editing.commitTransaction',
-    (description?: string) => getTransactionManager().commitTransaction(description),
+    (description?: string) => commitTransaction(description),
   ),
   new DefinePropertyRule(
     'Editing.cancelTransaction',
-    () => getTransactionManager().cancelTransaction(),
+    () => cancelTransaction(),
   ),
   new DefinePropertyRule(
     'Editing.doActions',
-    (cb: () => void, description?: string) => {
-      const transactionManager = getTransactionManager();
-      transactionManager.beginTransaction();
-      try {
-        cb();
-        transactionManager.commitTransaction(description);
-      } catch (e) {
-        transactionManager.cancelTransaction();
-        throw e;
-      }
-    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    <T>(cb: () => T extends Promise<any> ? never : T, description?: string) => doActions(cb, description)
   ),
 ];
