4ec87410e77ffcaca39e6087c05597ca0d80003d
David Slutsky <21009377+davidsl4@users.noreply.github.com>
2026-06-16 02:08:16 +0300

    feat: implement DataModel.BigJunctions.deleteBigJunction method (#32)

--- packages/patch-datamodel--bigjunctions/src/index.ts
+++ packages/patch-datamodel--bigjunctions/src/index.ts
@@ -3,10 +3,12 @@ import addBigJunctionRules from './rules/add-big-junction.rules.js';
 import configureArtifactsRules from './rules/configure-artifacts.rules.js';
 import updateBigJunctionRules from './rules/update-big-junction.rules.js';
 import updateBigJunctionAddressRules from './rules/update-big-junction-address.rules.js';
+import deleteBigJunctionRules from './rules/delete-big-junction.rules.js';
 
 export default [
   ...configureArtifactsRules,
   ...addBigJunctionRules,
+  ...deleteBigJunctionRules,
   ...updateBigJunctionRules,
   ...updateBigJunctionAddressRules,
 ] as SdkPatcherRule[];
---
+++ packages/wme-utils/src/lib/scrape-delete-object-action.ts
@@ -0,0 +1,42 @@
+import type { Model } from 'backbone';
+import { createEmptyDataModel } from './create-empty-data-model.js';
+import type { DataModel } from './data-model.js';
+import { runWithModel } from './run-with-model.js';
+import { WmeSDK } from 'wme-sdk-typings';
+import { MethodInterceptor } from '@wme-enhanced-sdk/method-interceptor';
+import { getWindow } from '@wme-enhanced-sdk/utils';
+import { captureActions } from './capture-actions.js';
+
+let cachedDataModel: DataModel | null = null;
+
+function getEditingMediator() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const window = getWindow<{ W: any }>();
+  return window.W.editingMediator;
+}
+
+export function scrapeDeleteObjectAction(sdk: WmeSDK, object: Model, actionName: string) {
+  const dataModel = cachedDataModel ??= createEmptyDataModel();
+  if (!dataModel) throw new Error('Failed to create empty data model');
+
+  dataModel.repos['venues'].objects['__object'] = object;
+  const [action] = runWithModel(dataModel, () => {
+    const isNewInterceptor = MethodInterceptor.mockReturnValueOnce(object, 'isNew', true);
+    const isEditingAllowedInterceptor = MethodInterceptor.mockReturnValueOnce(getEditingMediator(), 'isEditingAllowed', true);
+
+    try {
+      return captureActions(() => {
+        sdk.DataModel.Venues.deleteVenue({ venueId: '__object' });
+      }, { model: dataModel });
+    } finally {
+      isNewInterceptor.restore();
+      isEditingAllowedInterceptor.restore();
+    }
+  });
+  delete dataModel.repos['venues'].objects['__object'];
+  if (action.actionName !== actionName) {
+    throw new Error(`Unexpected action discovered when tried to resolve ${actionName} action: ${action.actionName}`);
+  }
+
+  return action.constructor;
+}
